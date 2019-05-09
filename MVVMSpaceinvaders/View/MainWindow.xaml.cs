c67fce62722119c025dccbe184100723337157cd
Nocciiii <47415855+Nocciiii@users.noreply.github.com>
2019-05-09 14:43:39 +0200

    ENDLICH KONNTEN WIR DIESEN ROTZ UPLOADEN

---
+++ MVVMSpaceinvaders/View/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace View
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Canvas.SetLeft(Panzer, p.X);
+        }
+
+        private void Panzer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            schuss();
+        }
+        
+    }
+}
---
+++ MVVMSpaceinvaders/ViewModel/VMMainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Threading;
+
+namespace ViewModel
+{
+    public class VMMainWindow
+    {
+
+        private void schuss()
+        {
+            Schuss neuerSchuss = new Schuss();
+            Thread s = new Thread(new ParameterizedThreadStart(neuerSchuss.feuer));
+            s.Start(lockObject);
+        }
+    }
+}
