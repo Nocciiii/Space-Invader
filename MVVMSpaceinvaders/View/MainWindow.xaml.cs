5ae3359e720941cc82007b9722e531a68cd6694e
Nocciiii <47415855+Nocciiii@users.noreply.github.com>
2019-05-13 12:29:50 +0200

    Ausmerzen des Bugs das Spieler nicht angezeigt wird

--- MVVMSpaceinvaders/View/MainWindow.xaml.cs
+++ MVVMSpaceinvaders/View/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace View
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
+    /// </summary>5
     public partial class MainWindow : Window
     {
         private Spieler spieler;
@@ -32,15 +32,14 @@ namespace View
         {
             this.spieler = new Spieler();
             this.img = new Image();
+            spielfeld.Children.Add(img);
             img.Source = new BitmapImage(spieler.Aussehen);
             double left = spielfeld.ActualWidth/2;
-            spielfeld.Children.Add(img);
-            Canvas.SetLeft(img, left);
-            Canvas.SetTop(img, 15);
-            img.Height = 10;
-            img.Width = 25;
-
-
+            img.Height = 20;
+            img.Width = 35;
+            Canvas.SetLeft(img, 50);
+            Canvas.SetBottom(img, 15);
+            img.Visibility = Visibility.Visible;
         }
 
        
